9a418575065f467d98815fc7fbe6827a9c447419
Sriram Thiagarajan <sri.sjc@gmail.com>
2023-10-09 21:23:56 +0530

    update footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,14 +1,30 @@
 <footer>
-	Built by <a href="https://twitter.com/tsriram" rel="noopener" target="_blank">Sriram</a>
+	Built with&nbsp;<a href="https://kit.svelte.dev/" rel="noopener" target="_blank">SvelteKit</a
+	>&nbsp;and&nbsp;
+	<a href="https://www.partykit.io/" rel="noopener" target="_blank">PartyKit</a>&nbsp;by&nbsp;<a
+		href="https://twitter.com/tsriram"
+		class="name"
+		rel="noopener"
+		target="_blank">Sriram</a
+	>
 </footer>
 
 <style>
 	footer {
+		display: flex;
 		padding: 2rem;
+		align-items: center;
+		justify-content: center;
+		margin-top: 4rem;
+		font-size: 90%;
 	}
 
 	footer a {
 		color: var(--footer-link-color);
 		text-decoration: none;
+	}
+
+	footer a:hover {
+		text-decoration: underline;
 	}
 </style>
