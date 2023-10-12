bfd61e5a15ed36d679339d6dea2e4f63cfdc2cd4
Sriram Thiagarajan <sri.sjc@gmail.com>
2023-10-12 22:24:53 +0530

    update meta tags and social image

--- src/lib/components/MetaTags.svelte
+++ src/lib/components/MetaTags.svelte
@@ -1,14 +1,28 @@
 <svelte:head>
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5" />
+
 	<meta property="og:title" content="Connect 4" />
-	<meta name="description" content="Connect 4 - two player online game" />
-	<meta property="og:description" content="Connect 4 - two player online game" />
+	<meta
+		property="og:description"
+		content="Play Connect 4 Online - Challenge friends or players worldwide in this classic strategy game. Connect four in a row to claim victory!"
+	/>
 	<meta name="og:image" content="https://connect4.live/images/social.png" />
 	<meta property="og:url" content="https://conenct4.live" />
+	<meta property="og:type" content="website" />
+	<meta property="og:locale" content="en_US" />
+	<meta property="og:title-prefix" content="Play now:" />
 
+	<meta
+		name="description"
+		content="Play Connect 4 Online - Challenge friends or players worldwide in this classic strategy game. Connect four in a row to claim victory!"
+	/>
 	<meta name="twitter:card" content="summary_large_image" />
 	<meta name="twitter:site" content="@tsriram" />
 	<meta name="twitter:image" content="https://connect4.live/images/social.png" />
 	<meta name="theme-color" content="#243447" />
+	<meta
+		name="keywords"
+		content="Connect 4, online game, multiplayer, strategy, board game, gaming, multiplayer Connect 4, classic game, four in a row, online multiplayer, play Connect 4, web-based game, challenge friends"
+	/>
 </svelte:head>
