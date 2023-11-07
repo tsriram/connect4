08aea1d90631eb86ff92b8b09da85fdf7290f5dc
Sriram Thiagarajan <sri.sjc@gmail.com>
2023-11-07 21:37:49 +0530

    add umami analytics
    will remove GA after confirming this works

---
+++ src/lib/components/UmamiAnalytics.svelte
@@ -0,0 +1,11 @@
+<!-- https://umami.is/docs/tracker-configuration -->
+<svelte:head>
+  <script
+    async
+    defer
+    src="https://analytics.eu.umami.is/script.js"
+    data-website-id="b83a21e6-f0f3-4d81-b080-7624a90b6a73"
+    data-do-not-track="true"
+    data-domains="connect4.live"
+  ></script>
+</svelte:head>
